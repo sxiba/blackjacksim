1b05c0c8634437a14a63ec803b8526db7073d034
achang-1 <achang19@asu.edu>
2023-11-21 03:04:18 -0700

    add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+
+
+blackjack = 21 
+class cards():
+    def __init__(self) -> None:
+        pass
+    
+class deal():
+    def __init__(self) -> None:
+        pass
+    
+if __name__ == "__main__": 
+    pass 
+   
