9dac271e3d00e0907b548321f4c1f59013e2c6f5
JamesFink5690 <104277729+JamesFink5690@users.noreply.github.com>
2023-11-21 17:42:51 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import random 
 
 
-blackjack = 21 
+blackjack = 21
+suites = ["Spades", "Clubs", "Diamonds", "Hearts"]
 class cards():
     def __init__(self) -> None:
-        pass
+        deck = cards.initDeck()
+
+    def initDeck():
+        deck = [dict() for x in range(52)] 
+        i = 0
+        for suite in suites: 
+            for j in range(1,14):
+                deck[i] = cards.createCard(
+                    suite,
+                    j,
+                    j > 10,
+                    i
+                )
+                i += 1
+
+        return deck            
+
+    def createCard(suite, value, royalty, index):
+        return {
+            "Suite": suite, 
+            "Value": value, 
+            "Royalty": royalty, 
+            "Deck position": index
+            }
+
+
+
     
 class deal():
     def __init__(self) -> None:
         pass
     
 if __name__ == "__main__": 
-    pass 
+    cards.initDeck()
    
