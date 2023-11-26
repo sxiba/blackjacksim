c27665064d298ece63cc44fccf54089523859998
achang-1 <achang19@asu.edu>
2023-11-26 01:54:54 -0700

    add changes

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ import time
 hand = 250 
 
 blackjack = 21 
+
+# create card object
 class Cards():
     def __init__(self) -> None:
-        suits = {"Hearts", "Spades", "Clubs", "Diamonds"}
-        deck = {
+        self.suit = None
+        self.card = None 
+        self.value = None
+        __suits__ = ["Hearts", "Spades", "Clubs", "Diamonds"]
+        __cards__ = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
+        __card_values__ = {
             "A" : 1, 
             "2" : 2,  
             "3" : 3,  
@@ -36,30 +42,29 @@ class Deal():
         pass
     # player gets another card if they choose 
     def hit():
+
         pass
+    
+# Start game loop. 
 def start_game():
+    # game state starts as false, while user hasn't quit, continue game. 
     quit = False
-    if quit == True: 
-        print("here")
-        exit()
-    else: 
-        while not quit: 
-            print("Welcome to blackjack.")
-            print("--------------------")
-            print("Will you play?\n\n")
-            print("Hit (H) --- Quit (Q)")
-            user_input = input("Hit (H) --- Quit (Q)")
-            if user_input.lower() == "h": 
-                print("Your pool: $" + str(hand))
-                Deal.hit()
-                print("")
-            elif user_input.lower() == "q":
-                print("Thanks for playing!")
-                quit = True
-            else:
-                print("Input not recognized. Try again.")
-        
-    pass 
+    while not quit: 
+        print("Welcome to blackjack.")
+        print("--------------------")
+        print("Will you play?\n\n")
+        print("Hit (H) --- Quit (Q)")
+        user_input = input("Hit (H) --- Quit (Q)\n")
+        if user_input.lower() == "h": 
+            print("\nYour money: $" + str(hand))
+            Deal.hit()
+            print("")
+        elif user_input.lower() == "q":
+            print("Thanks for playing!")
+            quit = True
+        else:
+            print("\nInput not recognized. Try again.\n\n")
+    exit()
    
 if __name__ == "__main__": 
     start_game()
