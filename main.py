1142ea01b7808f428c970009651784bcc406e0a5
sxiba <albcha@protonmail.com>
2023-12-08 10:59:21 -0700

    get hit-stand-quit functional

--- main.py
+++ main.py
@@ -61,53 +61,78 @@ def print_cards(hand):
     __cards = []
     for card in hand: 
         __cards.append(card.card + " " + card.suit)
-    return __cards
+    print(__cards)
+    print("value: " + str(check_hand_value(hand)))
+    print()
 
-def deal():
+def deal_hands(game_deck):
 # need to have check for consistent 21 or if dealer busts or if player busts. 
 # deals hand, player gets 2 cards, dealer gets 2 but second draw is hidden. 
 
     # initialize cards 
     # 52 cards from game deck 
     # new list of pulled cards, removing from game deck 
-    game_deck = Deck.create_deck()
+    
     player_hand = [draw_card(game_deck), draw_card(game_deck)]
     print("")
-    print(print_cards(player_hand), )
+    print_cards(player_hand)
+
     dealer_hand = [draw_card(game_deck), draw_card(game_deck)]
-    print(print_cards(dealer_hand))
-    exit()
-    dealer_hand = [random.choice(game_deck), random.choice(game_deck)]
-    print(print_cards(dealer_hand[0]), print_cards(dealer_hand[1]))
-    # if either cards are ace, show 1 or 11
-    if player_hand[0] == 1: 
-        print(player_hand[0][2], player_hand[1][2])
-        print("Hand is either " + player_hand[0][2] + " or 11")
-        
-        
-         
-    pass
+    print_cards(dealer_hand)
+    print("what player sees: " + dealer_hand[0].card + " " + dealer_hand[0].suit)
+
+    return player_hand, dealer_hand
+
+# either we are checking value more than we print or vise 
+def check_hand_value(hand):
+    return (sum(int(card.value) for card in hand))
+
 # Start game loop. 
 def start_game():
     # game state starts as false, while user hasn't quit, continue game. 
     quit = False
+    print("Welcome to blackjack.")
+    print("--------------------\n\n")
+    print("\nYour money: $" + str(hand))
+    game_deck = Deck.create_deck()
+    player_hand, dealer_hand = deal_hands(game_deck)
     while not quit: 
-        print("Welcome to blackjack.")
-        print("--------------------\n\n")
-        # print("Will you play?\n\n")
-        
-        # hit, stand, double, split, quit program 
-        user_input = input("Hit (H) --- Quit (Q)\n")
-        if user_input.lower() == "h": 
-            print("\nYour money: $" + str(hand))
-            deal()
 
-            print("")
-        elif user_input.lower() == "q":
-            print("Thanks for playing!")
-            quit = True
-        else:
-            print("\nInput not recognized. Try again.\n\n")
+        # hit, stand, double, split, quit program 
+        user_input = input("Hit (H) --- Stand (S) --- Quit (Q)\n")
+        match user_input.lower():
+            case 'h': 
+                player_hand.append(draw_card(game_deck))
+                print_cards(player_hand)
+                
+                if check_hand_value(player_hand) > 21:
+                    print("You have: " + str(check_hand_value(player_hand)) + ". Sorry!")
+                    quit = True
+                # random sets dealer card
+            case 's':
+                print("\n\nDealer's turn:\n")
+                while check_hand_value(dealer_hand) < 16:
+                    dealer_hand.append(draw_card(game_deck))
+                    if check_hand_value(dealer_hand) > 21:
+                        print("Dealer has: " + str(check_hand_value(player_hand)) + ". You win!")
+                print_cards(dealer_hand)
+
+                if check_hand_value(player_hand) < check_hand_value(dealer_hand):
+                    print("Dealer wins.")
+
+                elif check_hand_value(player_hand) > check_hand_value(dealer_hand):
+                    print("You win!")
+                else: 
+                    print("Push. Your bet is returned.")
+                quit = True 
+                # dealer does their thing, compares, then exits the dealt session
+                # in stand, we check for 21 after dealer thinks they have a decent draw 
+            case 'q':
+                print("Thanks for playing!")
+                quit = True
+            case '_':
+                print("\nInput not recognized. Try again.\n\n")
+ 
     exit()
    
 if __name__ == "__main__": 
