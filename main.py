d4fbb69a2068ee729adf2c74e5389c20de34d5eb
sxiba <albcha@protonmail.com>
2023-12-09 09:57:51 -0700

    remove conflict

--- main.py
+++ main.py
@@ -29,31 +29,7 @@ class Deck():
         }
     
     def __init__(self) -> None:
-        deck = cards.initDeck()
-
-    def initDeck():
-        deck = [dict() for x in range(52)] 
-        i = 0
-        for suite in suites: 
-            for j in range(1,14):
-                deck[i] = cards.createCard(
-                    suite,
-                    j,
-                    j > 10,
-                    i
-                )
-                i += 1
-
-        return deck            
-
-    def createCard(suite, value, royalty, index):
-        return {
-            "Suite": suite, 
-            "Value": value, 
-            "Royalty": royalty, 
-            "Deck position": index
-            }
-
+        pass
 
 
     
