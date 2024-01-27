2ecc76fda17cc5d3d663e1020fbd90d22a9aeb46
sxiba <albcha@protonmail.com>
2024-01-27 16:19:19 -0700

    test split interaction

---
+++ blackjack.py
@@ -0,0 +1,103 @@
+#! usr/bin/python3
+import random
+
+# Create card object 
+class Cards(): 
+    def __init__(self, suit, card, value) -> None:
+        self.suit = suit
+        self.card = card 
+        self.value = value
+        pass
+    
+    # prints a singular card
+    def print_card(self):
+        return self.card + " " + self.suit
+    
+    
+# create card object, n amount of decks played at once 
+class Deck():
+    # n sets of deck 
+    suits = ["of Hearts", "of Spades", "of Clubs", "of Diamonds"]
+    cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
+    card_values = {
+        "A" : 11, 
+        "2" : 2,  
+        "3" : 3,  
+        "4" : 4,  
+        "5" : 5,  
+        "6" : 6,  
+        "7" : 7,  
+        "8" : 8,  
+        "9" : 9,  
+        "10" : 10,  
+        "J" : 10,  
+        "Q" : 10,  
+        "K" : 10  
+        }
+
+    def __init__(self) -> None:
+        self.deck = Deck.create_deck()
+        pass
+    def create_deck():
+        """Creates n number of decks where n is a random integer (1-8)."""
+        _deck = []
+        for suit in Deck.suits: 
+            for card in Deck.cards:
+                _deck.append(Cards(suit, card, Deck.card_values[card]))
+        return _deck*random.randint(1,8)
+    
+    def draw_card(self):
+        """Draw a card from a deck"""
+        drawn = random.choice(self.deck)
+        self.deck.remove(drawn)
+        return drawn
+    def deal_hand(self):
+        # returns a list of cards (two)
+        hand = [self.draw_card() for i in range(0,2)]
+        return hand
+        
+        
+# class Hand():
+#     def __init__(self) -> None:
+#         pass
+    
+class Player():
+    def __init__(self, bal) -> None:
+        self.hand = None
+        self.balance = bal
+        self.bet_amount = None
+        self.hand_value = None
+        pass
+    
+    def update_balance(self,bal):
+        self.balance += bal
+        
+    def set_bet(self,bet):
+        self.bet_amount = bet
+ 
+    def set_hand(self, hand):
+        self.hand = hand 
+        self.update_value()
+    
+    def append_card_to_hand(self, card):
+        self.hand.append(card)
+        self.update_value()
+        
+    def update_value(self):
+        ace_count, total_value = 0, 0
+        for card in self.hand:
+            value = card.value
+            ace_count += (value == 11)
+            total_value += value
+
+        #Aces are valued 1 if they would cause a bust if valued at 11
+        if total_value > 21 and ace_count > 0:
+            reductions_needed = int((total_value - 21)/10) + 1 # refer to pep 8?
+            total_value -= reductions_needed*10 if reductions_needed <= ace_count else ace_count*10
+        self.hand_value = total_value
+        
+    def print_hand(self):
+        hand = []
+        for card in self.hand:
+            hand.append(card.print_card())
+        return hand
--- main.py
+++ main.py
@@ -7,21 +7,79 @@ blackjack = 21
 
 
 if __name__ == "__main__": 
+    
     print("Welcome.\n" + '-'*80)
+    
     player_balance = int(input("How much would you like to deposit (number)? \n"))
     player = Player(player_balance)
     dealer = Player(float('inf')) #or None)
+    
+    split = False
+    num_hands = 1
     game = True
+    
+    def player_decision():
+        while 1:
+            user_input = input("Hit (H) --- Stand (S) --- Double (D) --- Quit (Q)\n")            
+            match user_input.lower():
+                case 'h': 
+                    player.append_card_to_hand(game_deck.draw_card())
+                    print(player.hand_value)
+                    print(player.print_hand())
+                    if player.hand_value > 21: 
+                        return
+                case 'd': 
+                    player.append_card_to_hand(game_deck.draw_card())
+                    return
+                case 's':
+                    print("You choose to stand.")
+                    return    
+                case 'q':
+                    print("Thanks for playing!\n")
+                    exit()
+                case _:
+                    print("Input not recognized. Try again.\n")
+
     while game:
         game_deck = Deck()
                 
-        player_bet = int(input("How much would you like to bet (number)? \n"))
+        player_bet = int(input("How much would you like to bet (number)? \n\n"))
         player.set_bet = player_bet
         print("You bet: " + str(player_bet) + "\n")
         
         player.set_hand(game_deck.deal_hand())
         dealer.set_hand(game_deck.deal_hand())
         
-        print("Your hand: " + str(player.print_hand()) )        
+        print("Your hand: " + str(player.print_hand()))        
+    
+        # blackjack
+        if player.hand_value >= 21:
+            print("You win! You have blackjack!")
+            game = False
+        # player.hand[0] = Cards("of Hearts", 'A', 11)
+        # player.hand[1] = Cards("of Hearts", 'A', 11)
+        # check for pairs/split
+        if player.hand[0].value == player.hand[1].value:
+     
+            player_split = str(input("Would you like to split (Enter Y/N)? \n\n")).lower()
+            if player_split == "y":
+                num_hands = 2 
+                print("You split.")
+                hands = [player.hand[0]], [player.hand[1]]
+                print(hands)
+            else:
+                hands = [player.hand]
+        for i in range(0, num_hands):
+            print(f"Your {i+1} thstrd hand: ")
+            player.hand = hands[i]
+            
+            player_decision()
         
-        game = False
+        # dealer hand
+        if dealer.hand_value >= 17: 
+            # stand
+            pass
+        else:
+            pass
+        
+        # TODO: payout 
